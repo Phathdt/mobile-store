a23ce04764241526dc798ed628f3ebf184771148
Phathdt <phathdt.leviateam@gmail.com>
2018-04-22 01:37:14 +0700

    done api signin

--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import { Button, FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
+
+injectTapEventPlugin();
 
 class SignInPage extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      email: '',
+      username: '',
       password: ''
     };
   }
 
   validateForm() {
-    return this.state.email.length > 0 && this.state.password.length > 0;
+    return this.state.username.length > 0 && this.state.password.length > 0;
   }
 
   handleChange = event => {
@@ -21,20 +24,41 @@ class SignInPage extends Component {
     });
   };
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
+    try {
+      let res = await fetch('https://mobile-store-612.herokuapp.com/login', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          userName: this.state.username,
+          password: this.state.password
+        })
+      });
+      if (res.status === 401) {
+        alert('something went wrong');
+      } else {
+        let resJson = await res.json();
+        localStorage.setItem('token', resJson.Authorization);
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   render() {
     return (
       <div className="Login">
         <form onSubmit={this.handleSubmit}>
-          <FormGroup controlId="email" bsSize="large">
-            <ControlLabel>Email</ControlLabel>
+          <FormGroup controlId="username" bsSize="large">
+            <ControlLabel>Username</ControlLabel>
             <FormControl
               autoFocus
-              type="email"
-              value={this.state.email}
+              type="text"
+              value={this.state.username}
               onChange={this.handleChange}
             />
           </FormGroup>
