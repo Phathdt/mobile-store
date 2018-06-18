9bf857ec3ec5431498e8753501d001fbbc792027
Phathdt <phathdt.leviateam@gmail.com>
2018-06-18 09:41:21 +0700

    change user page

--- src/pages/customer/UserPage.js
+++ src/pages/customer/UserPage.js
@@ -4,11 +4,19 @@ import UserPageHeader from './UserPageHeader.js'
 import UserPageBody from './UserPageBody.js'
 
 class UserPage extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      token: this.props.token
+    }
+  }
+
   render() {
     return (
       <div>
-        <UserPageHeader/>
-        <UserPageBody/>
+        <UserPageHeader />
+        <UserPageBody token={this.state.token} />
       </div>
     )
   }
--- src/pages/customer/UserPageBody.js
+++ src/pages/customer/UserPageBody.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
-import Slider from "react-slick";
+import React, { Component } from 'react'
+import Slider from 'react-slick'
 
 import Api from 'Api'
 
 import UserPageFooter from './UserPageFooter.js'
 import SideMenu from './SideMenu'
-import CartPage from './CartPage.js';
+import CartPage from './CartPage.js'
 import '../../styles/customs/customer.css'
+import swal from 'sweetalert2'
 
-
-
-
-class UserPageBody extends Component{  
-    constructor(props) {
+class UserPageBody extends Component {
+  constructor(props) {
     super(props)
 
     this.state = {
@@ -57,100 +55,139 @@ class UserPageBody extends Component{
       data: response.content
     })
   }
-    render(){
-        var settings = {
-            autoPlay: true,
-            infinite: true,
-            autoplaySpeed: 1000,
-            speed: 500,
-        };
-        
-        return (
-            <div className="centerContent">
-                <div className="row">
-                    <div className="col-lg-9">
-                        <Slider {...settings}>
-                            <div>
-                                <img src={require("../../styles/images/slider1.png")}/>
-                            </div>
-                            <div>
-                                <img src={require("../../styles/images/slider2.png")}/>
-                            </div>
-                            <div>
-                                <img src={require("../../styles/images/slider3.png")}/>
-                            </div>
-                            <div>
-                                <img src={require("../../styles/images/slider4.png")}/>
-                            </div>
-                        </Slider>
-                    </div>
-                    <div className="col-lg-3 vJustifySpace">
-                        <div className="cr-widget  card text-white">
-                            <div className="card-body">
-                                <div className="card-title">Đảm bảo uy tín</div>
-                                <div className="card-subtitle">Đổi trả miễn phí nếu sản phẩm lỗi</div>
-                            </div>
-                        </div>
-                        <div className="cr-widget  card text-white">
-                            <div className="card-body header-widget">
-                                <div className="card-title">Trả góp 0%</div>
-                                <div className="card-subtitle">Áp dụng cho sản phẩm Oppo</div>
-                            </div>
-                        </div>
-                        <div className="cr-widget  card text-white">
-                            <div className="card-body header-widget">
-                                <div className="card-title">Bảo hành 2 năm</div>
-                                <div className="card-subtitle">Cho tất cả các sản phẩm điện thoại của Apple</div>
-                            </div>
-                        </div>
-                        <div className="cr-widget  card text-white">
-                            <div className="card-body header-widget">
-                                <div className="card-title">Giao hàng miễn phí</div>
-                                <div className="card-subtitle">Cho các đơn hàng trên 5 triệu đồng</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-lg-12" style={{'margin-bottom':'15px'}}>
-                        <img src={require("../../styles/images/promotion.png")} style={{'max-width': '1100px'}}/>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-lg-3" >
-                        <SideMenu/> 
-                    </div>
-                    <div className="col-lg-9">
-                        <div className="row">
-                            {this.state.data.map(i=>(
-                                <div className="col-lg-4">
-                                <div className="product-image-wrapper">
-                                    <div class="productinfo">
-                                        <img src={i.images[0].imageURL} />
-                                        <p>{i.name}</p>
-                                    </div>
-                                    <div class="product-overlay">
-                                            <div class="overlay-content">
-                                                <a href={`/variant/details/${i.variantId}`}>
-                                                    <h2>Giá: {i.pricesold}</h2>
-                                                    <p>{i.name}</p>
-                                                </a>
-                                                <a href="/cart" class="btn btn-default add-to-cart">
-                                                    Thêm vào giỏ
-                                                </a>
-                                            </div>
-                                    </div>
-                                </div>
-                            </div>
-                            ))}
-                        </div>
-                       
-                    </div>
-                </div>
-                <UserPageFooter/>
-            </div>
-        )
 
+  addToCart = async variantId => {
+    if (this.state.token == undefined) {
+      swal('Cancelled', 'Your need to sign in', 'error')
+    } else {
+      let cart = JSON.parse(localStorage.getItem('cart') || '[]')
+
+      let variant = cart.find(t => t.variantID == variantId)
+
+      if (variant) {
+        variant.countNumber += 1
+        variant.total =
+          parseInt(variant.countNumber) * parseInt(variant.priceEachUnit)
+      } else {
+        let item = this.state.data.find(t => t.variantId == variantId)
+        cart.push({
+          countNumber: 1,
+          priceEachUnit: item.pricesold,
+          variantID: item.variantId,
+          src: item.images[0].imageURL,
+          total: item.pricesold
+        })
+      }
+      await localStorage.setItem('cart', JSON.stringify(cart))
     }
+  }
+
+  render() {
+    var settings = {
+      autoPlay: true,
+      infinite: true,
+      autoplaySpeed: 1000,
+      speed: 500
+    }
+
+    return (
+      <div className="centerContent">
+        <div className="row">
+          <div className="col-lg-9">
+            <Slider {...settings}>
+              <div>
+                <img src={require('../../styles/images/slider1.png')} />
+              </div>
+              <div>
+                <img src={require('../../styles/images/slider2.png')} />
+              </div>
+              <div>
+                <img src={require('../../styles/images/slider3.png')} />
+              </div>
+              <div>
+                <img src={require('../../styles/images/slider4.png')} />
+              </div>
+            </Slider>
+          </div>
+          <div className="col-lg-3 vJustifySpace">
+            <div className="cr-widget  card text-white">
+              <div className="card-body">
+                <div className="card-title">Đảm bảo uy tín</div>
+                <div className="card-subtitle">
+                  Đổi trả miễn phí nếu sản phẩm lỗi
+                </div>
+              </div>
+            </div>
+            <div className="cr-widget  card text-white">
+              <div className="card-body header-widget">
+                <div className="card-title">Trả góp 0%</div>
+                <div className="card-subtitle">Áp dụng cho sản phẩm Oppo</div>
+              </div>
+            </div>
+            <div className="cr-widget  card text-white">
+              <div className="card-body header-widget">
+                <div className="card-title">Bảo hành 2 năm</div>
+                <div className="card-subtitle">
+                  Cho tất cả các sản phẩm điện thoại của Apple
+                </div>
+              </div>
+            </div>
+            <div className="cr-widget  card text-white">
+              <div className="card-body header-widget">
+                <div className="card-title">Giao hàng miễn phí</div>
+                <div className="card-subtitle">
+                  Cho các đơn hàng trên 5 triệu đồng
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-lg-12" style={{ 'margin-bottom': '15px' }}>
+            <img
+              src={require('../../styles/images/promotion.png')}
+              style={{ 'max-width': '1100px' }}
+            />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-lg-3">
+            <SideMenu />
+          </div>
+          <div className="col-lg-9">
+            <div className="row">
+              {this.state.data.map(i => (
+                <div className="col-lg-4">
+                  <div className="product-image-wrapper">
+                    <div class="productinfo">
+                      <img src={i.images[0].imageURL} />
+                      <p>{i.name}</p>
+                    </div>
+                    <div class="product-overlay">
+                      <div class="overlay-content">
+                        <a href={`/variant/details/${i.variantId}`}>
+                          <h2>Giá: {i.pricesold}</h2>
+                          <p>{i.name}</p>
+                        </a>
+                        <a
+                          onClick={() => {
+                            this.addToCart(i.variantId)
+                          }}
+                          class="btn btn-default add-to-cart"
+                        >
+                          Thêm vào giỏ
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+        <UserPageFooter />
+      </div>
+    )
+  }
 }
 export default UserPageBody
